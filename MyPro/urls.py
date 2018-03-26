aa2ab588a271131a0c93e52131d57ec4404b0824
tnhoang <tnhoang088@gmail.com>
2018-03-26 23:52:36 +0700

    add accounts to settings

--- MyPro/urls.py
+++ MyPro/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('boards.urls')),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^a$', include('django.contrib.auth.urls')),
     
     
 ]
